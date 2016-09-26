f22f0dc83da417e0f3a05c3a907e243098a699ce
achi11es <akhileshkontham@gmail.com>
2016-09-26 16:19:48 -0500

    3 commit

--- index.js
+++ index.js
@@ -22,31 +22,11 @@ app.get('/webhook/', function (req, res) {
 
 app.get('/', function (req, res) {
 
-    //res.send('Hello World! This is the bot\'s root endpoint!');
-    res.send(req.query['hub.challenge'])
+    res.send('Hello World! This is the bot\'s root endpoint!');
+    //res.send(req.query['hub.challenge'])
 });
-/*
+
 app.post('/webhook/', function (req, res) {
-
-    var messaging_events = req.body.entry[0].messaging;
-
-    for (var i = 0; i < messaging_events.length; i++) {
-
-        var event = req.body.entry[0].messaging[i];
-        var sender = event.sender.id;
-
-        if (event.message && event.message.text) {
-            var text = event.message.text;
-
-            sendTextMessage(sender, "Echo: " + text.substring(0, 200));
-        }
-    }
-
-    res.sendStatus(200);
-
-});*/
-
-app.post('/', function (req, res) {
 
     var messaging_events = req.body.entry[0].messaging;
 
