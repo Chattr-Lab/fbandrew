4a75907868454ef8e70eadd34e94723015cf4eb6
achi11es <akhileshkontham@gmail.com>
2016-10-05 17:12:56 -0500

    basic text parsing added

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function getRandomInt(min, max) {
 function sendTextMessage(sender, input) {
 
     var messageData=input;
-    
+    console.log(input);
     var greeting =["Hey!","Hello!","Hi there!"];
     var random = ["I didn't quite get it","I'm too smart to reply to that, try something else","try '@sport news' to get latest sport news"]
     if (input == 'greetings') {
